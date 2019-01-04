a73f0c99a7baeb959825a6c5e31384902a00475c
Deacon <Michocan@Carloss-MacBook-Pro.local>
2019-01-04 09:27:05 -0700

    tile builder online, still laying out on grid, no captions yet, both coming soon.

---
+++ scripts/tiling.js
@@ -0,0 +1,68 @@
+
+
+
+var Tile = function(height,imageSrc,imageAltAttr,tileType,productPrice,verdictImgSrc,verdictImgAlt,month,date,time) {
+	this.imageSrc = imageSrc;
+	this.imageAltAttr = imageAltAttr;
+	this.tileType = tileType;
+	this.verdictImgSrc = verdictImgSrc;
+	this.verdictImgAlt = verdictImgAlt;
+	this.month = month;
+	this.date = date;
+	this.time = time;
+	this.productPrice = productPrice;
+	// this.listingCaption = listingCaption;
+	// this.linkDest = linkDest;
+	this.height = height;
+};
+
+Tile.prototype.layTile = function() {
+	var tile = document.createElement('div');
+	tile.classList.add('tile');
+	tile.style.height = this.height + "px";
+
+	var img = document.createElement('img');
+	img.setAttribute('src', this.imageSrc);
+	img.classList.add('tile__background');
+	img.setAttribute('alt', this.imageAltAttr);
+	
+	var corner = document.createElement('div');
+	corner.classList.add('tile__upper-corner');
+	
+	if(this.tileType === "product") {
+		var price = document.createElement('span');
+		price.classList.add('tile__upper-corner__copy');
+		price.textContent = "$" + this.productPrice;
+		corner.appendChild(price);
+	} else if(this.tileType === "review") {
+		var verdict = document.createElement('img');
+		verdict.classList.add('tile__upper-corner__glyph');
+		verdict.setAttribute('src', this.verdictImgSrc);
+		verdict.setAttribute('alt', this.verdictImgAlt);
+		corner.appendChild(verdict);
+	} else if(this.tileType === "event") {
+		var month = document.createElement('p');
+		month.textContent = this.month;
+		month.classList.add("tile__upper-corner__month");
+		var date = document.createElement('p');
+		date.textContent = this.date;
+		date.classList.add("tile__upper-corner__date");
+		var time = document.createElement('p');
+		time.textContent = this.time;
+		time.classList.add("tile__upper-corner__time");
+
+		corner.appendChild(date);
+		// var breakOne = document.createElement('br');
+		// corner.appendChild(breakOne);
+		corner.appendChild(month);
+		// var breakTwo = document.createElement('br');
+		// corner.appendChild(breakTwo);
+		corner.appendChild(time);
+	} else {
+		console.log("invalid tile type");
+	}
+
+	tileBox.appendChild(tile);
+	tile.appendChild(img);
+	tile.appendChild(corner);
+};
