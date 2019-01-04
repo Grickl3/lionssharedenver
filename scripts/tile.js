ee076d2a57b06b62eef1ef13bfc4098fda68cc45
Deacon <Michocan@Carloss-MacBook-Pro.local>
2019-01-03 17:13:21 -0700

    tile layer is close to almost one day not being broken

--- scripts/tile.js
+++ scripts/tile.js
@@ -4,90 +4,160 @@
 /* ===================== TILE SET ======================== */
 
 		// EXAMPLE TILE
-		// {
-		// "listingTitle": "Example Title", // Internal purposes only
-		// "imageSrc": "img/name.png",
-		// "imageAltAttr": "Example Alt",
-		// "imageHeight": "",
-		// "tileType": "", // review, product, event
-		// "reviewVerdict": "up", //up, down, fire, shit, weird
-		// "eventDate": {"month": "", "date": "", "time": ""},
-		// "productPrice": "",
-		// "Listing Caption": "",
-		// "linkDest": ""
-		// }
+			// {
+			// "listingTitle": "Example Title", // Internal purposes only
+			// "imageSrc": "../img/name.png",
+			// "imageAltAttr": "Example Alt",
+			// "imageHeight": "",
+			// "tileType": "", // review, product, event
+			// "reviewVerdict": "up", //up, down, fire, shit, weird
+			// "eventDate": {"month": "", "date": "", "time": ""},
+			// "productPrice": "",
+			// "listingCaption": "",
+			// "linkDest": ""
+			// }
+		// EXAMPLE TILE MARKUP
+			// <div class="tile">
+			// 	<img class="tile__background" src="img/b-n-w.jpg" alt="black and white outfit">
+			// 	<div class="tile__upper-corner">  
+			// 		<span class="tile__upper-corner__copy">$75</span>  
+			// 		<img class="tile__upper-corner__glyph" src="img/thumbsup.png" alt="">
+			//		<span class="tile__upper-corner__copy">NOV<br>12<br>9pm</span>
+			// 	</div><!-- UPPER CORNER -->  
+			// 	<div class="tile__stripe"> 
+			// 		<span class="tile__stripe__copy">extra long T-shirt: white</span>  
+			// 	</div><!-- STRIPE -->  
+			// </div><!-- TILE -->
 
-	var tileSet = [];
-
-	
-	// <div class="tile">
-	// 	<img class="tile__background" src="img/b-n-w.jpg" alt="black and white outfit">
-	// 	<div class="tile__upper-corner">  
-	// 		<span class="tile__upper-corner__copy">$75</span>  
-	// 		<img class="tile__upper-corner__glyph" src="img/thumbsup.png" alt="">
-	//		<span class="tile__upper-corner__copy">NOV<br>12<br>9pm</span>
-	// 	</div><!-- UPPER CORNER -->  
-	// 	<div class="tile__stripe"> 
-	// 		<span class="tile__stripe__copy">extra long T-shirt: white</span>  
-	// 	</div><!-- STRIPE -->  
-	// </div><!-- TILE -->
-
-	function tileBuild(tileIndex) {
-
-		var cornerContent = "";
-		var reviewGlyph = "";
-		var reviewGlyphAlt = "";
-
-		function determineCorner(tileIndex) {
-			if(tileIndex.tileType === review) {
-				
-				function getGlyph(tileIndex.reviewVerdict) {
-					if(tileIndex.reviewVerdict === up) {
-						reviewGlyph = "img/thumbsup.png";
-						reviewGlyphAlt = ' alt="thumbs down"';
-					} else if (tileIndex.reviewVerdict === fire) {
-						reviewGlyph = "img/fire.png";
-						reviewGlyphAlt = ' alt="fire"';
-					} else if (tileIndex.reviewVerdict === down) {
-						reviewGlyph = "img/thumbsdown.png";
-						reviewGlyphAlt = ' alt="thumbs down"';
-					} else if (tileIndex.reviewVerdict === shit) {
-						reviewGlyph = "img/shit.png";
-						reviewGlyphAlt = ' alt="shit"';
-					} else {
-						console.log("invalid review, motherfucker.")
-					}
-				};
-
-				cornerContent = '"<img class="tile__upper-corner__glyph" src="' +
-				reviewGlyph + '"' + reviewGlyphAlt + '>';
-			} else if(tileIndex.tileType === product) {
-				cornerContent = '"<span class="tile__upper-corner__price">' +
-				tileIndex.productPrice + '</span>';
-			} else if(tileIndex.tileType === event) {
-				cornerContent = '<span class="tile__upper-corner__month">' + 
-				tileIndex.eventDate.month + '</span><br>' + 
-				'<span class="tile__upper-corner__date">' + 
-				tileIndex.eventDate.date + '</span><br>' +
-				'<span class="tile__upper-corner__time">' + 
-				tileIndex.eventDate.time + '</span>';
-			} else {
-				console.log(tileIndex + " invalid tile type, bitch.");
+	var tileSet = [
+		{
+			"listingTitle": "21 Savage review", // Internal purposes only
+			"imageSrc": "../img/21-savage.jpg",
+			"imageAltAttr": "21 Savage",
+			"imageHeight": "",
+			"tileType": "product", // review, product, event
+			"reviewVerdict": "", //up, down, fire, shit, weird
+			"eventDate": {"month": "", "date": "", "time": ""},
+			"productPrice": "12",
+			"listingCaption": "",
+			"linkDest": ""
+			},
+			{
+			"listingTitle": "Long Shirt", // Internal purposes only
+			"imageSrc": "../img/b-n-w.jpg",
+			"imageAltAttr": "black and white outfit",
+			"imageHeight": "",
+			"tileType": "product", // review, product, event
+			"reviewVerdict": "", //up, down, fire, shit, weird
+			"eventDate": {"month": "", "date": "", "time": ""},
+			"productPrice": "$75",
+			"listingCaption": "Extra Long T-Shirt",
+			"linkDest": ""
+			},
+			{
+			"listingTitle": "Tokyo Punk Look", // Internal purposes only
+			"imageSrc": "../img/box-print.jpg",
+			"imageAltAttr": "box-print",
+			"imageHeight": "",
+			"tileType": "product", // review, product, event
+			"reviewVerdict": "", //up, down, fire, shit, weird
+			"eventDate": {"month": "", "date": "", "time": ""},
+			"productPrice": "$800",
+			"listingCaption": "Tokyo Punk Look",
+			"linkDest": ""
 			}
-		};
+	];
 
-		determineCorner(tileIndex);
+	var tileContent = "";
 
-		var tileMarkUp = '<div class="tile">' +
-			'<img class="tile__background" src="' + tileIndex.imageSrc +
-			'" alt="' + tileIndex.imageAltAttr + '">' +
-			'<div class="tile__upper-corner">' + cornerContent +
+	function layTiles() {
+		
+		// var tileMarkUp = "";
 
-		;
+		for(i=0; i<=tileSet.length; i++) {
+			// var tileIndex = tileSet[i];
+			var tileImage = tileSet[i].imageSrc;
+			var tileImageAlt = tileSet[i].imageAltAttr;
 
-		var
+			// console.log(tileIndex);
 
-	}
+			// ATTEMPT ONE 
+
+				// function tileBuild(tileIndex) {
+
+					// var cornerContent = "";
+					// var reviewGlyph = "";
+					// var reviewGlyphAlt = "";
+
+					// function determineCorner(tileIndex) {
+
+					// 	if(tileIndex.tileType === "review") {
+					// 		var verdict = tileIndex.reviewVerdict;
+
+					// 		function getGlyph(verdict) {
+					// 			if(tileIndex.verdict === up) {
+					// 				reviewGlyph = "img/thumbsup.png";
+					// 				reviewGlyphAlt = ' alt="thumbs down"';
+					// 			} else if (tileIndex.verdict === fire) {
+					// 				reviewGlyph = "img/fire.png";
+					// 				reviewGlyphAlt = ' alt="fire"';
+					// 			} else if (tileIndex.verdict === down) {
+					// 				reviewGlyph = "img/thumbsdown.png";
+					// 				reviewGlyphAlt = ' alt="thumbs down"';
+					// 			} else if (tileIndex.verdict === shit) {
+					// 				reviewGlyph = "img/shit.png";
+					// 				reviewGlyphAlt = ' alt="shit"';
+					// 			} else {
+					// 				console.log("invalid review, motherfucker.")
+					// 			}
+					// 		};
+
+					// 		getGlyph();
+
+					// 		cornerContent = '"<img class="tile__upper-corner__glyph" src="' +
+					// 			reviewGlyph + '"' + reviewGlyphAlt + '>';
+					// 	} else if(tileIndex.tileType === "product") {
+					// 		cornerContent = '"<span class="tile__upper-corner__price">' +
+					// 			tileIndex.productPrice + '</span>';
+					// 	} else if(tileIndex.tileType === "event") {
+					// 		cornerContent = '<span class="tile__upper-corner__month">' + 
+					// 			tileIndex.eventDate.month + '</span><br>' + 
+					// 			'<span class="tile__upper-corner__date">' + 
+					// 			tileIndex.eventDate.date + '</span><br>' +
+					// 			'<span class="tile__upper-corner__time">' + 
+					// 			tileIndex.eventDate.time + '</span>';
+					// 	} else {
+					// 		console.log(tileIndex + " invalid tile type, bitch.");
+					// 	}
+					// };
+
+					// determineCorner(tileIndex);
+
+					// var tileMarkUp = '<div class="tile">' +
+						// '<img class="tile__background" src="' + tileIndex.imageSrc +
+						// '" alt="' + tileIndex.imageAltAttr + '">' +
+						// '<div class="tile__upper-corner">' + cornerContent + '</div><!-- UPPER CORNER -->' +
+						// '<div class="tile__stripe">' +
+						// '<span class="tile__stripe__copy">' + tileIndex.listingCaption +'</span>' + 
+						// '</div><!-- STRIPE -->' + '</div><!-- TILE -->'
+					// ;
+					// console.log(tileMarkUp);
+				// };
+
+				// tileContent += tileMarkUp;
+				// console.log(tileContent);
+
+			// ATTEMPT TWO
+
+				tileContent += '<div class="tile">' + 
+				'<img class="tile__background" src="' + tileImage +
+				'" alt="' + tileImageAlt + '">' +
+				'</div><!-- TILE -->';
+
+		}
+
+		tileBox.innerHTML = tileContent;
+	};
 
 
 	
